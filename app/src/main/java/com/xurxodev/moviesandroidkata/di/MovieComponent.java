4b21b6338a713fdf1d4df75fb474d7ae51311b09
malmorox <marcos.almorox@gmail.com>
2026-04-07 18:05:35 +0200

    Eliminando la parte de detalle por errores que surgian

--- app/src/main/java/com/xurxodev/moviesandroidkata/di/MovieComponent.java
+++ app/src/main/java/com/xurxodev/moviesandroidkata/di/MovieComponent.java
@@ -1,6 +1,5 @@
 package com.xurxodev.moviesandroidkata.di;
 
-import com.xurxodev.moviesandroidkata.view.fragment.MovieDetailFragment;
 import com.xurxodev.moviesandroidkata.view.fragment.MoviesFragment;
 import javax.inject.Singleton;
 import dagger.Component;
@@ -9,5 +8,4 @@ import dagger.Component;
 @Component(modules = {AppModule.class, RepositoryModule.class, ImageModule.class, PresenterModule.class})
 public interface MovieComponent {
     void inject(MoviesFragment moviesFragment);
-    void inject(MovieDetailFragment movieDetailFragment);
 }
--- app/src/main/java/com/xurxodev/moviesandroidkata/di/PresenterModule.java
+++ app/src/main/java/com/xurxodev/moviesandroidkata/di/PresenterModule.java
@@ -1,6 +1,5 @@
 package com.xurxodev.moviesandroidkata.di;
 
-import com.xurxodev.moviesandroidkata.MovieDetailContract;
 import com.xurxodev.moviesandroidkata.MoviesContract;
 import com.xurxodev.moviesandroidkata.presenter.MoviesPresenter;
 import dagger.Module;
@@ -9,14 +8,9 @@ import dagger.Provides;
 @Module
 public class PresenterModule {
     private MoviesContract.View view;
-    private MovieDetailContract.View detailView;
 
     public PresenterModule(MoviesContract.View view) {
         this.view = view;
-    }
-
-    public PresenterModule(MovieDetailContract.View detailView) {
-        this.detailView = detailView;
     }
 
 
@@ -27,16 +21,6 @@ public class PresenterModule {
 
     @Provides
     MoviesContract.Presenter providePresenter(MoviesPresenter presenter) {
-        return presenter;
-    }
-
-    @Provides
-    MovieDetailContract.View provideDetailView() {
-        return detailView;
-    }
-
-    @Provides
-    MovieDetailContract.Presenter provideDetailPresenter(MovieDetailPresenter presenter) {
         return presenter;
     }
 }
